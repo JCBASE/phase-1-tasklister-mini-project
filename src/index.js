cd66389ac4aef0d406ffd2d8b307bfa83a8151de
Jessica Campos <Jessica.c@bronxsoftware.org>
2023-06-05 20:49:56 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    buildNewTaskDescription(event.target.children[1].value);
+
+  });
+
 });
+
+function buildNewTaskDescription(ntd){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${ntd} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
